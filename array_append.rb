5b2904ba6e6d0742b589b83b59624c43a218f699
Lara Finnegan <lcfinnegan@gmail.com>
2015-02-01 14:00:34 -0600

    added reverse append method

--- array_append.rb
+++ array_append.rb
@@ -7,11 +7,11 @@ def append(array, n)
 end
 
 def reverse_append(array, n)
-	return array if n > 0
+	return array if n < 0
+	reverse_append(array, n-1)
 	array << n
-	reverse_append(array, n+1)
 end
 
 
 p append([], 5)
-p reverse_append([], -10)
+p reverse_append([], 10)
