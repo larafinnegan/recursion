e60ae5135b807f22070cefa4b5803c4aadacafd2
Lara Finnegan <lcfinnegan@gmail.com>
2015-02-04 22:04:50 -0600

    refactored merge method

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,21 +2,15 @@ def mergesort(array)
   return array if array.length <= 1
     array1 = array[0..((array.length/2)-1)]
     array2 = array[array1.length..-1]
-    mini_array1 = mergesort(array1)
-    mini_array2 = mergesort(array2)
-    newarray = merge(mini_array1, mini_array2)
+    a = mergesort(array1)
+    b = mergesort(array2)
+    newarray = merge(a, b)
 end
 
 def merge(a, b)
   sorted = []
-  while a.length > 0 && b.length > 0
-    if a[0] <= b[0]
-      sorted << a[0]
-      a = a[1..-1]
-    else
-      sorted << b[0]
-      b = b[1..-1]
-    end
+  until a.empty? || b.empty?
+    a[0] <= b[0] ? sorted << a.shift : sorted << b.shift
   end
     sorted += a += b
 end
@@ -24,5 +18,5 @@ end
 
 array = Array.new(500)
 array.map! {|x| x = rand(300)}
-p mergesort(array)
+p mergesort([1,5,2,3,2])
 
